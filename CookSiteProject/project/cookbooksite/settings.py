e68b013ca4d4ccadf277b8ed0a97f4aa5f82e7c3
Malina55555 <malinovka55555@yandex.ru>
2023-10-08 20:58:01 +0300

    подключение бд и создание модели рецепта в приложении

--- CookSiteProject/project/cookbooksite/settings.py
+++ CookSiteProject/project/cookbooksite/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'recipes',
 ]
 
 MIDDLEWARE = [
@@ -76,7 +78,7 @@ WSGI_APPLICATION = 'cookbooksite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'C:/Users/User/DiskD/cookbooksite.db', #BASE_DIR / 'db.sqlite3', #os.path.join(BASE_DIR, 'address.db'), - так прописано в https://habr.com/ru/articles/240463/#chapter_01
     }
 }
 
--- CookSiteProject/project/recipes/models.py
+++ CookSiteProject/project/recipes/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class Recipe(models.Model):
+    recipe_name = models.CharField(
+        max_length=255,
+    )
+    recipe_ingredients = models.CharField(
+        max_length=255,
+    )
+    recipe_process = models.CharField(
+        max_length=255,
+    )
+    #email = models.EmailField()
+
+    def __str__(self):
+        return ' '.join([
+            self.recipe_name,
+            self.recipe_ingredients,
+            self.recipe_process
+        ])
